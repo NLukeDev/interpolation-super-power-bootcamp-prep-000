99662164ff140564d6ccaf111ead7271879ccd8b
Nekwan Luke <contact@nekwanluke.com>
2019-06-27 12:37:13 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,33 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
-puts 
+  
+ colors = [] 
+  
+puts "#{colors[]}"
 end
 
+
+
+R: red
+O: orange
+Y: yellow
+G: green
+B: blue
+I: Indigo
+V: violet 
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+
+
+bob_name           = "Bob"
+bob_age            = 46
+bob_occupation     = "Juggler"
+stefani_name       = "Stefani"
+stefani_age        = 49
+stefani_occupation = "Firefighter"
+ 
+puts "Name: #{bob_name}, Age: #{bob_age}, Occupation: #{bob_occupation}"
+
+puts "Name: #{stefani_name}, Age: #{stefani_age}, Occupation:
+#{stefani_occupation}"
